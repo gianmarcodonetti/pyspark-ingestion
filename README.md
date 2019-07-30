fb741069b8bb12d18ec01590cb0359dbb840b810
Gianmarco Donetti <Gianmarco.DONETTI@loreal.com>
2019-07-30 09:56:50 +0200

    first stable

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+First services developed:
+1. Batch ingestion for a single table
+2. Batch ingestion for all tables in a defined system
---
+++ setup.py
@@ -0,0 +1,48 @@
+import os
+import re
+
+import pip
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+with open(os.path.join(os.path.dirname(__file__), 'pyspark-ingestion', 'version.py')) as v_file:
+    VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+class OverrideInstall(install):
+    """
+    Emulate sequential install of pip install -r requirements.txt
+    """
+
+    def run(self):
+        # install requirements first
+        install_reqs = parse_requirements(
+            './requirements/common.txt', session=False)
+        reqs = [str(ir.req) for ir in install_reqs]
+        for req in reqs:
+            pip.main(["install", req])
+
+        install.run(self)
+
+
+config = {
+    'name': 'pyspark-ingestion',
+    'description': 'Python package for LOI',
+    'author': 'G. Donetti, J. Radaelli',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
+    'version': VERSION,
+    'packages': find_packages(),
+    'scripts': [],
+    'cmdclass': {'install': OverrideInstall},
+    'test_suite': 'nose.collector',
+    'tests_require': ['nose'],
+}
+
+setup(**config)
